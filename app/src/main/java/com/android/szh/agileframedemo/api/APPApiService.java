54d30b0c7b9ab04d4c26c135c4d39e2a151d6ff8
sunzhonghao <sunzhonghao06472@hellobike.com>
2018-12-10 19:20:18 +0800

    增加自定解析对象的实例

--- app/src/main/java/com/android/szh/agileframedemo/api/APPApiService.java
+++ app/src/main/java/com/android/szh/agileframedemo/api/APPApiService.java
@@ -1,12 +1,16 @@
 package com.android.szh.agileframedemo.api;
 
 import com.android.szh.common.config.UrlConfig;
+import com.android.szh.common.http.BaseEntity;
+
+import java.util.Map;
 
 import io.reactivex.Observable;
 import okhttp3.ResponseBody;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.Path;
+import retrofit2.http.QueryMap;
 
 /**
  * Created by sunzhonghao on 2018/5/16.
@@ -26,4 +30,13 @@ public interface APPApiService {
     @Headers({UrlConfig.FLAG_MULTIPLE_BASE_URL_KEY + UrlConfig.FLAG_MULTIPLE_BASE_URL_A})
     @GET("/v2/book/{id}")
     Observable<ResponseBody> testSwitchBaseUrl(@Path("id") int id);
+
+
+    /**
+     * 自定解析对象实例
+     *
+     * Object为任意数据类型（要和json对象数据结构相对应）
+     */
+    @GET("/xxxx/xxxx.do")
+    Observable<BaseEntity<Object>> getXXX(@QueryMap Map<String, String> map);
 }
